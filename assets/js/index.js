d735d0587dcbcd0ca16ffd4ed5a91ccac82ad1ec
XimenesJu <ju.cx@hotmail.com>
2022-12-11 22:10:04 -0300

    creating remaining pages html + adding functions to switch between to pages + css 80% complete for the whole site

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,24 @@
-var button = document.getElementById('inputbutton')
+function goToStep1() {
+  location.replace("../steps/index.html")
+    
+}
 
 function goToStep2() {
   location.replace("../steps/step2.html")
     
 }
+
+function goToStep3() {
+  location.replace("../steps/step3.html")
+    
+}
+
+function goToStep4() {
+  location.replace("../steps/step4.html")
+    
+}
+
+function goToConfirmation() {
+  location.replace("../steps/confirmation.html")
+}
+
