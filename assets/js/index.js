e6c552b218260ce2b72107c397264012fbabed8a
XimenesJu <ju.cx@hotmail.com>
2022-12-17 00:17:30 -0300

    correcting index html path after moving it to folder outside + starting writing js function to show values on step 4

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,3 +22,14 @@ function goToConfirmation() {
   location.replace("../steps/confirmation.html")
 }
 
+function plan1() {
+  
+  var p1n = document.getElementById('plan1name')
+  var p1p = document.getElementById('plan1price')
+  var p1s = document.getElementById('planselected')
+  var p1sp = document.getElementById('planselectedprice')
+
+
+  p1s.innerHTML = p1n
+  p1sp.innerHTML = p1p
+}
